2e39099cb2e3a3a0cd8f368f6cec1d4543330e9e
chrisn <maflaja@gmail.com>
2014-09-18 17:42:45 +1000

    Added more comments to explain the reason for this code.

--- project/SbtIvyFix.scala
+++ project/SbtIvyFix.scala
@@ -9,6 +9,12 @@ import sbt.ScalaVersion
 import sbt.Configurations.{ Compile }
 import sbt.Configurations
 
+/**
+ * The objective here is to make the artefacts published to the local repository by the publishLocal task
+ * compatible with the local resolver used by IvyDE.
+ * This is achieved by dropping the classifiers from the "doc" and "source" artefacts, and by adding
+ * an extra level directory to their paths to avoid clashing with the "jar" main artefact.
+ */
 object SbtIvyFix extends Build with PathExtra {
   lazy override val projects = Seq(root)
   lazy val root: Project = Project("xlstocsv", new File(".")) settings (
@@ -21,7 +27,7 @@ object SbtIvyFix extends Build with PathExtra {
     artifactPath in (Compile, packageSrc) <<= myArtifactPathSetting(artifact in (Compile, packageSrc)),
     artifactPath in (Compile, packageDoc) <<= myArtifactPathSetting(artifact in (Compile, packageDoc)))
 
-  // Lifted from Sbt source
+  // Lifted from the Sbt source artifactPathSetting() function in Defaults.scala
   def myArtifactPathSetting(art: SettingKey[Artifact]) = (crossTarget, projectID, art, scalaVersion in artifactName, scalaBinaryVersion in artifactName, artifactName) {
     (t, module, a, sv, sbv, toString) =>
       {
